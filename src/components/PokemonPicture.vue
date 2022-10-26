fccac4bb5735c54b9157f1aad0708a237f7f4d99
rossmery-garcia <garciarossmery@gmail.com>
2022-10-26 15:35:15 -0300

    Design of the PokemonPicture component

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,5 +1,18 @@
 <template>
-  <h3>Pokemon Picture</h3>
+  <div class="pokemon-container">
+    <img
+    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+    class="hidden-pokemon"
+    alt="Pokemon image"
+  >
+
+  <img
+    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+    class="fade-in"
+    alt="Pokemon image"
+  >
+  </div>
+
 </template>
 
 <script>
@@ -8,6 +21,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+/* Pokemon Picture */
+.pokemon-container {
+    height: 200px;
+}
+img {
+    height: 200px;
+    position: absolute;
+    right: 32%;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
 </style>
