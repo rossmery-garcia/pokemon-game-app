c31e3e74cd09adb5d40f7159a2dbcd70f24530ad
rossmery-garcia <garciarossmery@gmail.com>
2022-10-26 18:52:14 -0300

    getPokemonOptions method implemented

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -2,18 +2,36 @@
   <h1>¿Quién es este Pokémon?</h1>
 
   <PokemonPicture :pokemonId="4" :showPokemon="true" />
-  <PokemonOptions />
+  <PokemonOptions :pokemons="pokemonArr" />
 
 </template>
 
 <script>
-import PokemonPicture from '@/components/PokemonPicture.vue';
-import PokemonOptions from '@/components/PokemonOptions.vue';
+import PokemonPicture from '@/components/PokemonPicture';
+import PokemonOptions from '@/components/PokemonOptions';
+
+import getPokemonOptions from '@/helpers/getPokemonOptions';
+
+console.log( getPokemonOptions() );
 
 export default {
   components: {
     PokemonPicture,
     PokemonOptions
+  },
+  data() {
+    return {
+      pokemonArr: [],
+    }
+  },
+  methods: {
+    async mixPokemonArr() {
+      this.pokemonArr = await getPokemonOptions();
+      console.log(this.pokemonArr)
+    }
+  },
+  mounted() {
+    this.mixPokemonArr();
   }
 }
 </script>
