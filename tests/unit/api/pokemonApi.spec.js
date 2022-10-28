71fef49649ab72fdf12c8fad7649b93a2c14605f
rossmery-garcia <garciarossmery@gmail.com>
2022-10-28 02:07:38 -0300

    Unit test of the methods getPokemonOptions, getPokemons and getPokemonNames

--- tests/unit/api/pokemonApi.spec.js
+++ tests/unit/api/pokemonApi.spec.js
@@ -3,7 +3,7 @@ import pokemonApi from '@/api/pokemonApi';
 describe('Pokemon API', () => {
 
   test('Axios must be configured with Pokemon API', () => {
-    console.log(pokemonApi.defaults.baseURL)
+
     expect( pokemonApi.defaults.baseURL ).toBe('https://pokeapi.co/api/v2/pokemon');
   });
 })
